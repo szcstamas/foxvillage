81bfb5dcc0931b28b08efaec0b0b0cfb41a16ced
sztamas99 <98643734+sztamas99@users.noreply.github.com>
2022-08-29 13:59:41 +0200

    custom scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./styles/styles.css";
-import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import AnimatedRoutes from './AnimatedRoutes';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
