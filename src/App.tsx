3bfa079d4bfeee39fa6337cd80401f35e5098cb3
sztamas99 <98643734+sztamas99@users.noreply.github.com>
2022-09-01 09:28:14 +0200

    changing links to gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import "./styles/styles.css";
-import { HashRouter as Router } from "react-router-dom";
+// import { HashRouter as Router } from "react-router-dom";
 import AnimatedRoutes from './AnimatedRoutes';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
+    <>
 
       <Navbar />
       <AnimatedRoutes />
       <Footer />
 
-    </Router>
+    </>
   );
 }
 
