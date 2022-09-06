74c841040dfc88d06258167b86188bfa856605ce
sztamas99 <98643734+sztamas99@users.noreply.github.com>
2022-09-06 15:57:26 +0200

    changing thankyou page

--- src/components/ThankYou.tsx
+++ src/components/ThankYou.tsx
@@ -1,40 +1,46 @@
 import React from 'react'
 import { FlexCenter } from '../constants/Styles'
 import { Typography, Box } from '@mui/material';
-import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import PrimaryButton from './buttons/PrimaryButton';
+import { CardSectionStyles } from '../constants/Styles';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from '../constants/Theme';
 
 const ThankYou = () => {
     return (
-        <Box
-            component='div'
-            sx={{ height: '80vh', ...FlexCenter, textAlign: 'center', flexDirection: 'column', gap: 2 }}
-        >
-            <Typography
-                variant='h1'
-                component={motion.h1}
-                initial={{ opacity: 0, x: -100 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ delay: .5 }}
-            >Thank you!</Typography>
+        <ThemeProvider theme={theme}>
+            <Box
+                component='div'
+                sx={{ height: '80vh', ...FlexCenter, textAlign: 'center', flexDirection: 'column', gap: 2, ...CardSectionStyles }}
+            >
+                <Typography
+                    variant='h1'
+                    color='primary.main'
+                    component={motion.h1}
+                    initial={{ opacity: 0, x: -100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ delay: .5 }}
+                    sx={{ lineHeight: '5rem', marginBottom: { xs: 3, md: 2 } }}
+                >Thank you!</Typography>
 
-            <Typography
-                paragraph={true}
-                component={motion.p}
-                initial={{ opacity: 0, x: 100 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ delay: 1 }}
-            >Your message was sent to us! We will reply to you soon! ❤</Typography>
+                <Typography
+                    paragraph={true}
+                    component={motion.p}
+                    initial={{ opacity: 0, x: 100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ delay: 1 }}
+                >Your message was sent to us! We will reply to you soon! ❤</Typography>
 
-            <PrimaryButton
-                text='Back to homepage!'
-                link='/#/'
-                icon='home'
-            />
+                <PrimaryButton
+                    text='Back to homepage!'
+                    link='/#/'
+                    icon='home'
+                />
 
 
-        </Box>
+            </Box>
+        </ThemeProvider>
     )
 }
 
