e50f8bebc796b6a9a77bbd6a2a01ceaddba07862
sztamas99 <98643734+sztamas99@users.noreply.github.com>
2022-09-06 15:24:01 +0200

    adding thankyou page

--- src/components/ThankYou.tsx
+++ src/components/ThankYou.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { FlexCenter } from '../constants/Styles'
 import { Typography, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import PrimaryButton from './buttons/PrimaryButton';
 
 const ThankYou = () => {
     return (
@@ -9,9 +11,28 @@ const ThankYou = () => {
             component='div'
             sx={{ height: '90vh', ...FlexCenter, textAlign: 'center', flexDirection: 'column', gap: 2 }}
         >
-            <Typography variant='h1'>Thank you!</Typography>
-            <Typography paragraph={true}>Your message was sent to us! We will reply to you soon! ❤</Typography>
-            <Link to='/'>Back to homepage</Link>
+            <Typography
+                variant='h1'
+                component={motion.h1}
+                initial={{ opacity: 0, x: -100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ delay: .5 }}
+            >Thank you!</Typography>
+
+            <Typography
+                paragraph={true}
+                component={motion.p}
+                initial={{ opacity: 0, x: 100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ delay: 1 }}
+            >Your message was sent to us! We will reply to you soon! ❤</Typography>
+
+            <PrimaryButton
+                text='Back to homepage!'
+                link='/#/'
+                icon='home'
+            />
+
 
         </Box>
     )
